7f0a90b579d26133da67b0c25227f8d52dc6acb5
Arnold Ballesteros <arnoldballesteros@arnolds-mac.local>
2016-01-25 23:45:21 -0500

    These files are within the Flicks app awaiting to be reviewed

---
+++ Movie Reviewer/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  Movie Reviewer
+//
+//  Created by Arnold Ballesteros on 1/25/16.
+//  Copyright © 2016 Arnold Ballesteros. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
